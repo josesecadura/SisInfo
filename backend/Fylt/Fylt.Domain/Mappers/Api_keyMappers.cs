a017a36247ab0241effccac402f3a53e4c69e701
José Secadura del Olmo <jsecadura1@gmail.com>
2025-10-24 18:21:01 +0200

    Carpetas para VOs, Mappers y Service de A a la E

---
+++ backend/Fylt/Fylt.Domain/Mappers/Api_keyMappers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fylt.Domain.Mappers
+{
+    internal class Api_keyMappers
+    {
+    }
+}
